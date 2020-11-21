e1be6113b1f322a54845726ccd27047f48295026
JoniKristianK <JoniKristianK@github.com>
2020-11-21 13:01:44 +0200

    Updating state with axios from api.gihub

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <NavBar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
