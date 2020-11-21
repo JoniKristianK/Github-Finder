07589b4754f27d6599b1464f0c76d51c02e6f323
JoniKristianK <JoniKristianK@github.com>
2020-11-21 15:07:51 +0200

    Add Search component and functionality.. passing props up (searchUsers)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import './App.css';
 
 class App extends Component {
@@ -23,11 +24,25 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <NavBar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
