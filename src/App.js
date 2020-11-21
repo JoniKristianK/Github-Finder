609da42fd7d0694673abc284cde786e991944a24
JoniKristianK <JoniKristianK@github.com>
2020-11-21 10:46:43 +0200

    mapping list of users to UserItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import NavBar from './components/layout/NavBar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +9,9 @@ class App extends Component {
     return (
       <div className='App'>
         <NavBar />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
