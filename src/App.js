e78824a2a595c22083261991dcc233919041243e
JoniKristianK <joni.karilainen@gmail.com>
2020-11-22 21:42:38 +0200

    Create AlertState, lerteducer and Context

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ import About from './components/pages/About';
 import './App.css';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
   // useEffect(() => {
   //   setLoading(true);
 
@@ -30,39 +29,32 @@ const App = () => {
   //   setLoading(false);
   // }, []);
 
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => {
-      setAlert(null);
-    }, 4000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <NavBar />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={(props) => (
-                  <>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <NavBar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path='/'
+                  render={(props) => (
+                    <>
+                      <Search />
+                      <Users />
+                    </>
+                  )}
+                />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
